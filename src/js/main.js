485d8b0c317a810bc48ca1b10109726a002ea7c7
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2024-07-10 21:22:37 +0600

    completed forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import slider from "./modules/slider";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -7,4 +8,5 @@ window.addEventListener("DOMContentLoaded", () => {
     modals(); // Запускаем модальные окна.
     slider(".feedback-slider-item", "horizontal", ".main-prev-btn", ".main-next-btn"); //горизонтальный слайдер.
     slider(".main-slider-item", "vertical"); //вертикальный сладер.
+    forms(); // Запускаем формы обратной связи.
 });
