824b8be3f9389dbb96b43cbad67035b896d558d6
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2024-07-10 22:01:29 +0600

    completed phone mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import modals from "./modules/modals";
 import slider from "./modules/slider";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -9,4 +10,5 @@ window.addEventListener("DOMContentLoaded", () => {
     slider(".feedback-slider-item", "horizontal", ".main-prev-btn", ".main-next-btn"); //горизонтальный слайдер.
     slider(".main-slider-item", "vertical"); //вертикальный сладер.
     forms(); // Запускаем формы обратной связи.
+    mask("[name='phone']"); // маска для номера телефона.
 });
